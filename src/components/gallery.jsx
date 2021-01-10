4912b3050e46433e3a41cfca156cd4627264808c
mw <melvillewatford@gmail.com>
2021-01-10 10:10:23 +0100

    fetch images with graphql query

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,23 +1,36 @@
 import React from "react"
 import ImageGallery from "react-image-gallery"
-import images from "@/content/gallery.json"
+import { graphql, useStaticQuery } from "gatsby"
 
-const pictures = images.map(img => ({
-  original: "/images/galeria/" + img,
-}))
+const Gallery = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allImageSharp(filter: { fluid: { originalName: { regex: "/slide/" } } }) {
+        nodes {
+          fluid(maxWidth: 1920, quality: 80) {
+            originalName
+            srcWebp
+          }
+        }
+      }
+    }
+  `)
 
-export default class Gallery extends React.Component {
-  render() {
-    return (
-      <section id="galeria">
-        <h1 className="text--white">Galeria</h1>
-        <ImageGallery
-          items={pictures}
-          showThumbnails={false}
-          showIndex={true}
-          autoPlay={true}
-        ></ImageGallery>
-      </section>
-    )
-  }
+  return (
+    <section id="galeria">
+      <h1 className="text--white">Galeria</h1>
+      <ImageGallery
+        items={data.allImageSharp.nodes.map(img => ({
+          original: img.fluid.srcWebp,
+        }))}
+        showThumbnails={false}
+        showIndex={true}
+        autoPlay={true}
+        lazyLoad={true}
+        originalAlt="widok okolicy"
+      ></ImageGallery>
+    </section>
+  )
 }
+
+export default Gallery
--- src/components/heading.jsx
+++ src/components/heading.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Navigation from "@/components/navigation"
 import Logo from "@/components/logo"
+import { HeadingImage } from "@/queries/images/Heading"
 
 const Heading = () => {
   return (
     <section className="heading">
+      <HeadingImage></HeadingImage>
       <Navigation></Navigation>
       <header className="heading__title">
         <Logo></Logo>
