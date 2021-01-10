9b4f46d2583b955c0cd348920f1e2a65729bd79f
mw <melvillewatford@gmail.com>
2021-01-10 10:04:05 +0100

    fix typos/content change

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,10 +6,10 @@ const Contact = () => {
       <div className="col j-center">
         <h1 className="text--blue">Kontakt</h1>
         <address>
-          MBN Mazurskie biuro Nieruchomości <br />
+          MBN Mazurskie Biuro Nieruchomości <br />
           ul. Dąbrowskiego 13 <br />
           11-500 Giżycko <br />
-          +48 874 281 938 <br />
+          +48 87 4281938 <br />
           <a href="https://mbn.mazury.pl">www.mbn.mazury.pl</a> <br />
           <a href="mailto:mbn@mbn.mazury.pl">mbn@mbn.mazury.pl</a>
         </address>
@@ -20,6 +20,7 @@ const Contact = () => {
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
+        loading={"lazy"}
       ></iframe>{" "}
     </div>
   )
