190028f35ba68d2dd6086e66430f2d2b8efabfad
mw <melvillewatford@gmail.com>
2021-01-10 10:33:15 +0100

    add lang and description

--- src/components/seo.js
+++ src/components/seo.js
@@ -73,9 +73,9 @@ function SEO({ description, lang, meta, title }) {
 }
 
 SEO.defaultProps = {
-  lang: `en`,
+  lang: `pl`,
   meta: [],
-  description: ``,
+  description: `Działki do zabudowy na mazurach w pięknej lokalizacji.`,
 }
 
 SEO.propTypes = {
